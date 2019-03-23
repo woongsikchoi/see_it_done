d8b875a2a08bc94c017c853963e3ffbd9e81ae6b
PerezLamed <perez.lamed.vanniekerk@gmail.com>
2019-03-23 21:10:15 +0200

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,27 @@
+c:
+cd c:\projects
+
+cd lamed_test
+start npm run update
+cd c:\projects
+echo ----------------------------------
+
+cd lamed_io
+start npm run update
+cd c:\projects
+echo ----------------------------------
+
+cd lamed_folder
+start npm run update
+cd c:\projects
+echo ----------------------------------
+
+cd lamed_flowchart
+start npm run update
+cd c:\projects
+echo ----------------------------------
+
+cd see_it_done
+start npm run update
+cd c:\projects
+echo ----------------------------------
