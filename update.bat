eda4d77139a79ec4e4adc6711ec4943c8928d24c
perezLamed <perez.lamed.vanniekerk@gmail.com>
2019-03-28 13:46:40 +0200

    Update update.bat

--- update.bat
+++ update.bat
@@ -8,29 +8,34 @@ pause
 cls
 
 c:
-cd %path%
+cd %path1%
 
 cd lamed_test
 start npm run update
 cd %path1%
-echo ----------------------------------
+echo ----------------------------------[lamed_test
 
 cd lamed_io
 start npm run update
 cd %path1%
-echo ----------------------------------
+echo ----------------------------------[lamed_io
 
 cd lamed_folder
 start npm run update
 cd %path1%
-echo ----------------------------------
+echo ----------------------------------[lamed_folder
 
 cd lamed_flowchart
 start npm run update
 cd %path1%
-echo ----------------------------------
+echo ----------------------------------[lamed_flowchart
+
+cd lamed_learn
+start npm run update
+cd %path1%
+echo ----------------------------------[lamed_learn
 
 cd see_it_done
 start npm run update
 cd %path1%
-echo ----------------------------------
+echo ----------------------------------[see_it_done
