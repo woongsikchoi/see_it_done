818cfe0e7fea88d7a7b4af5851452cb9b85cc8c2
PerezLamed <perez.lamed.vanniekerk@gmail.com>
2019-07-13 10:26:56 +0200

    add deepscan

--- src/badge/badge.js
+++ src/badge/badge.js
@@ -12,6 +12,7 @@ const { Ok, notOk, notOk_Then, Equal, notEqual, con, testAND, unZip } = _test //
 // con.useChalk(require('chalk'))
 // con.traceSet(0)
 
+require('lamed_string')
 const _badge = require('./lbadge')
 const _scan = require('./deepscan')
 
@@ -33,8 +34,8 @@ function github (data) {
  * Release badges
  */
 function releases (data) {
-  this.userName = data.userName
-  this.project = data.project
+  this.userName = data.userName.toLowerCase()
+  this.project = data.project.toLowerCase()
 
   this.npmRelease = _badge.npmRelease(this.userName, this.project)
   this.npmDownloads = _badge.npmDownload_Total(this.userName, this.project)
